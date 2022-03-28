b01dff54cfcd6591675a5732bf45ccda434ec2e6
Abhijith Tammanagari <23abhijith@gmail.com>
2022-03-27 23:44:47 -0400

    natural gas load

---
+++ dataGen.py
@@ -0,0 +1,15 @@
+import requests
+import json
+import pandas as pd
+import numpy as np
+apikey = "WnugwvU75RddvPG2NJh9wbarHU88ZyVgX4XL5EXV"
+states = [ 'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY' ];
+#states = ['AL']
+dataframe = pd.DataFrame()
+for state in states:
+    response = requests.get("https://api.eia.gov/series/?api_key={}&series_id=NG.N3010{}2.A".format(apikey, state))
+    data = response.json()
+    temp = pd.DataFrame(data['series'][0]['data'], columns=['year', 'consumption'])
+    temp['State'] = ['AL']*len(data['series'][0]['data'])
+    temp['Type'] = ['Natural Gas']*len(data['series'][0]['data'])
+    dataframe.append(temp, ignore_index=True)
